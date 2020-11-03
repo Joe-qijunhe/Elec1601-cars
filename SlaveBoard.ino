2cc4d7716e866a2ce3b98a90e371476fba426616
Qijun He <qijun.he1@outlook.com>
2020-11-03 16:02:13 +1100

    Add files via upload

---
+++ SlaveBoard.ino
@@ -0,0 +1,128 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>   //Software Serial Port
+
+#define RxD 7
+#define TxD 6
+#define ConnStatus A1
+
+#define DEBUG_ENABLED  1
+
+Servo servoLeft;
+Servo servoRight;
+int shieldPairNumber = 6;
+
+
+boolean ConnStatusSupported = true;   // Set to "true" when digital connection status is available on Arduino pin
+
+
+String slaveNameCmd = "\r\n+STNA=Slave";   // This is concatenated with shieldPairNumber later
+
+SoftwareSerial blueToothSerial(RxD,TxD);
+
+
+void setup() {
+    Serial.begin(9600);
+    blueToothSerial.begin(38400);                    // Set Bluetooth module to default baud rate 38400
+    
+    pinMode(RxD, INPUT);
+    pinMode(TxD, OUTPUT);
+    pinMode(ConnStatus, INPUT);
+    pinMode(10, INPUT);
+    pinMode(9, OUTPUT);
+    pinMode(3, INPUT);
+    pinMode(2, OUTPUT);
+    //  Check whether Master and Slave are already connected by polling the ConnStatus pin (A1 on SeeedStudio v1 shield)
+    //  This prevents running the full connection setup routine if not necessary.
+
+    if(ConnStatusSupported) Serial.println("Checking Slave-Master connection status.");
+
+    if(ConnStatusSupported && digitalRead(ConnStatus)==1)
+    {
+        Serial.println("Already connected to Master - remove USB cable if reboot of Master Bluetooth required.");
+    }
+    else
+    {
+        Serial.println("Not connected to Master.");
+        
+        setupBlueToothConnection();   // Set up the local (slave) Bluetooth module
+
+        delay(1000);                  // Wait one second and flush the serial buffers
+        Serial.flush();
+        blueToothSerial.flush();
+    }
+
+}
+
+void loop() {
+    char recvChar;
+    servoLeft.attach(13);
+    servoRight.attach(12);
+    while(1)
+    {
+        if(blueToothSerial.available())   // Check if there's any data sent from the remote Bluetooth shield
+        {
+            recvChar = blueToothSerial.read();
+            Serial.println(recvChar);
+            //forward
+            if(recvChar == "w"){
+              servoLeft.writeMicroseconds(1700);
+              servoRight.writeMicroseconds(1300);
+              delay(3000);
+            }
+            //backward
+            else if (recvChar == "s"){
+              servoLeft.writeMicroseconds(1300);
+              servoRight.writeMicroseconds(1700);
+              delay(3000);
+            }
+            //forward-left
+            else if (recvChar == "a"){
+              servoLeft.writeMicroseconds(1500);
+              servoRight.writeMicroseconds(1300);
+              delay(3000);
+            }
+            //forward-right
+            else if (recvChar == "d"){
+              servoLeft.writeMicroseconds(1700);
+              servoRight.writeMicroseconds(1500);
+              delay(3000);
+            }
+       }
+
+        if(Serial.available())            // Check if there's any data sent from the local serial terminal. You can add the other applications here.
+        {
+            recvChar  = Serial.read();
+            Serial.print(recvChar);
+            blueToothSerial.print(recvChar);
+        }
+    }
+}
+
+void setupBlueToothConnection()
+{
+    Serial.println("Setting up the local (slave) Bluetooth module.");
+
+    slaveNameCmd += shieldPairNumber;
+    slaveNameCmd += "\r\n";
+
+    blueToothSerial.print("\r\n+STWMOD=0\r\n");      // Set the Bluetooth to work in slave mode
+    blueToothSerial.print(slaveNameCmd);             // Set the Bluetooth name using slaveNameCmd
+    blueToothSerial.print("\r\n+STAUTO=0\r\n");      // Auto-connection should be forbidden here
+    blueToothSerial.print("\r\n+STOAUT=1\r\n");      // Permit paired device to connect me
+    
+    //  print() sets up a transmit/outgoing buffer for the string which is then transmitted via interrupts one character at a time.
+    //  This allows the program to keep running, with the transmitting happening in the background.
+    //  Serial.flush() does not empty this buffer, instead it pauses the program until all Serial.print()ing is done.
+    //  This is useful if there is critical timing mixed in with Serial.print()s.
+    //  To clear an "incoming" serial buffer, use while(Serial.available()){Serial.read();}
+
+    blueToothSerial.flush();
+    delay(2000);                                     // This delay is required
+
+    blueToothSerial.print("\r\n+INQ=1\r\n");         // Make the slave Bluetooth inquirable
+    
+    blueToothSerial.flush();
+    delay(2000);                                     // This delay is required
+    
+    Serial.println("The slave bluetooth is inquirable!");
+}
